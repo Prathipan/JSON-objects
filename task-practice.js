f8b2a9214631f12d861c03bd0b120ca1dfc8e9b1
Prathipan <83485567+Prathipan@users.noreply.github.com>
2021-12-23 22:43:13 +0530

    Add files via upload

---
+++ task-practice.js
@@ -0,0 +1,72 @@
+const scores = [
+    {
+      marks: 32,
+      name: "Yvette Merritt"
+    },
+    {
+      marks: 57,
+      name: "Lillian Ellis"
+    },
+    {
+      marks: 22,
+      name: "Mccall Carter"
+    },
+    {
+      marks: 21,
+      name: "Pate Collier"
+    },
+    {
+      marks: 91,
+      name: "Debra Beard"
+    },
+    {
+      marks: 75,
+      name: "Nettie Hancock"
+    },
+    {
+      marks: 20,
+      name: "Hatfield Hodge"
+    }
+  ];
+  // [32, 57, 22]
+  // Task 1
+  // Print the name list - As an array
+  // Expected Output
+  // ["Yvette Merritt", "Lillian Ellis", "Mccall Carter", "Pate Collier" ,
+  //  "Debra Beard", "Nettie Hancock",  "Hatfield Hodge"]
+
+  // for(let value in scores) {
+  //    console.log(scores[value].name);
+  // }
+  
+  
+  console.log(scores.map((person)=>person.name));
+  
+  
+  // Task 2
+  // >=40 pass.. find those student that passed (Use array method)
+  // Expected Output
+  // [{
+  //     marks: 57,
+  //     name: "Lillian Ellis"
+  //   },
+  //     {
+  //     marks: 91,
+  //     name: "Debra Beard"
+  //   },
+  //   {
+  //     marks: 75,
+  //     name: "Nettie Hancock"
+  //   }]
+    
+    console.log(scores.filter((person)=>person.marks >= 40));
+  // Task 3
+  //  Find all the names who failed the exams (Array methods)
+  // Expected Output
+  //   ["Yvette Merritt",
+  //   "Mccall Carter",
+  //   "Pate Collier",
+  //   "Hatfield Hodge"
+  // ];
+  console.log(scores.filter((person)=>person.marks <= 40).map((person)=>person.name));
+  
